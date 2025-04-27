c39406d95691f00e00fad4836d6ec8e4a2c49764
SaraLindholm <sara.lindholm88@gmail.com>
2025-04-27 22:05:12 +0200

    Byggt stöd för att visa bild + rubrik+datum för de tre senaste inlägget i footer. just nu utan styling dock

--- wp-content/themes/susannes-tavlor/footer.php
+++ wp-content/themes/susannes-tavlor/footer.php
@@ -1,8 +1,54 @@
 <footer id="footer">
   <section id="content">
-    <p>&copy; <?php echo date('Y'); ?> Susannes Tavlor</p>
+    <div>
+      <p>ArtBySusanne25 &copy; <?php echo date('Y'); ?></p>
+    </div>
   </section>
-  </footer>
-  <?php wp_footer(); ?>
+
+  <section>
+    <h4>Senaste inläggen</h4>
+    <?php
+    $reccent_posts = new WP_Query([
+      'posts_per_page' => 3,
+      'post_status' => 'publish',
+      'orderby' => 'date',
+    ]);
+    if ($reccent_posts->have_posts()) :
+      while ($reccent_posts->have_posts()) : $reccent_posts->the_post();
+    ?>
+
+        <div class="card" style="width: 18rem;">
+        <?php
+              if (has_post_thumbnail()) {
+                the_post_thumbnail('thumbnail', ['alt' => get_the_title()]);
+              } else {
+                // Hämta första bilden från innehållet
+                $content = get_the_content();
+                preg_match('/<img.+src=[\'"](?P<src>.+?)[\'"].*>/i', $content, $image);
+                if (!empty($image['src'])) {
+                  echo '<img class="wp-thumbnail" src="' . esc_url(get_template_directory_uri() . '/images/no-image.png') . '" alt="Standardbild">';
+                }
+              }
+              ?>
+
+          <div class="card-body">
+            <a href="<?php the_permalink() ?>">
+              <h5 class="card-title"><?php the_title() ?></h5>
+              <p class="card-text"><?php the_date() ?></p>
+            </a>
+          </div>
+        </div>
+      <?php
+      endwhile;
+      wp_reset_postdata();
+    else :
+      ?>
+      <p>Inga inlägg att visa</p>
+    <?php endif; ?>
+  </section>
+
+</footer>
+<?php wp_footer(); ?>
 </body>
+
 </html>
--- wp-content/themes/susannes-tavlor/functions.php
+++ wp-content/themes/susannes-tavlor/functions.php
@@ -3,22 +3,31 @@
 function susannes_tavlor_register_menus() {
   register_nav_menus([
     'main_menu' => 'Huvudmeny',
+    'footer_menu' => 'Footermeny',
   ]);
 }
 add_action('after_setup_theme', 'susannes_tavlor_register_menus');
 
+
+
+function susannes_tavlor_setup () {
+  add_theme_support('post-thumbnails');
+  add_theme_support('title-tag');
+}
+add_action('after_setup_theme', 'susannes_tavlor_setup');
+
 function susannes_tavlor_enqueue_scripts() {
 
-  // Lägg till Bootstrap CSS från CDN
+  // Stöd för Boostrap
   wp_enqueue_style('bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css', false, '4.3.1', 'all');
 
-  // Font Awesome från CDN (rekommenderat sätt)
+  // Font Awesome
 wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css', false, '6.5.0');
 
-  // Lägg till ditt eget tema CSS
+  //EGEN CSS
 wp_enqueue_style('theme-style', get_template_directory_uri() . '/css/style.css', [], filemtime(get_template_directory() . '/css/style.css'));
 
-  // Lägg till Bootstrap JS och dess beroende jQuery (måste vara i rätt ordning)
+  //  Bootstrap JS och dess beroende jQuery (måste vara i rätt ordning)
 wp_enqueue_script('jquery');
 wp_enqueue_script('bootstrap-js', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js', array('jquery'), '4.3.1', true);
 }
